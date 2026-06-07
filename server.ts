3a40bab4d2c45c3bdaf31fe3d78b354cbfbd1309
Agamani Mukherjee <agamanimukherjee2006@gmail.com>
2026-06-07 21:14:15 +0530

    fix: global crypto module injection polyfill

--- server.ts
+++ server.ts
@@ -1,10 +1,18 @@
+import 'crypto';
 import express, { Request, Response } from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 import path from 'path';
 import { fileURLToPath } from 'url';
 import Exam from './src/models/Exam.js'; // Adjust this path if your models folder is elsewhere
-
+import crypto from 'crypto';
+if (!globalThis.crypto) {
+    Object.defineProperty(globalThis, 'crypto', {
+        value: crypto,
+        writable: true,
+        configurable: true
+    });
+}
 // Recreate __dirname for ES Modules
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
